1cc6a0a375b29d1a92dba93d6e159e8136a4ec52
Davide Entratici <davideentratici@gmail.com>
2018-07-23 18:38:01 +0200

    Modificato Titolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Eliver Adventure
                 </div>
 
                 <div class="links">
