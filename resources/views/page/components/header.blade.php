fc929cc1eb7225420005d6530358e38fe2059d4e
Davide Entratici <davideentratici@gmail.com>
2018-07-30 15:27:02 +0200

    Diviso template file

---
+++ resources/views/page/components/header.blade.php
@@ -0,0 +1,171 @@
+
+
+<!-- Remove "navbar-sticky" class to make navigation bar scrollable with the page.-->
+<header class="site-header navbar-sticky">
+    <!-- Topbar-->
+    <div class="topbar d-flex justify-content-between">
+        <!-- Logo-->
+        <div class="site-branding d-flex"><a class="site-logo align-self-center" href="index.html"><img src="img/logo/logo.png" alt="Unishop"></a></div>
+        <!-- Search / Categories-->
+        <div class="search-box-wrap d-flex">
+            <div class="search-box-inner align-self-center">
+                <div class="search-box d-flex">
+                    <div class="btn-group categories-btn">
+                        <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"><i class="icon-menu text-lg"></i>&nbsp;Categories</button>
+                        <div class="dropdown-menu mega-dropdown">
+                            <div class="row">
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/01.jpg"><span class="text-gray-dark">Computers &amp; Accessories</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/02.jpg"><span class="text-gray-dark">Smartphones &amp; Tablets</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/03.jpg"><span class="text-gray-dark">TV, Video &amp; Audio</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/04.jpg"><span class="text-gray-dark">Cameras, Photo &amp; Video</span></a></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/05.jpg"><span class="text-gray-dark">Headphones</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/06.jpg"><span class="text-gray-dark">Wearable Electronics</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/07.jpg"><span class="text-gray-dark">Printers &amp; Ink</span></a></div>
+                                <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/08.jpg"><span class="text-gray-dark">Video Games</span></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <form class="input-group" method="get"><span class="input-group-btn">
+                  <button type="submit"><i class="icon-search"></i></button></span>
+                        <input class="form-control" type="search" placeholder="Search for anything">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- Toolbar-->
+        <div class="toolbar d-flex">
+            <div class="toolbar-item visible-on-mobile mobile-menu-toggle"><a href="#">
+                    <div><i class="icon-menu"></i><span class="text-label">Menu</span></div></a></div>
+            <div class="toolbar-item hidden-on-mobile"><a href="#">
+                    <div><i class="flag-icon"><img src="img/flags/EN.png" alt="English"></i><span class="text-label">Eng / Usd</span></div></a>
+                <ul class="toolbar-dropdown lang-dropdown">
+                    <li class="px-3 pt-1 pb-2">
+                        <select class="form-control form-control-sm">
+                            <option value="usd">$ USD</option>
+                            <option value="usd">€ EUR</option>
+                            <option value="usd">£ UKP</option>
+                            <option value="usd">¥ JPY</option>
+                        </select>
+                    </li>
+                    <li><a href="#"><i class="flag-icon"><img src="img/flags/FR.png" alt="Français"></i>&nbsp;Français</a></li>
+                    <li><a href="#"><i class="flag-icon"><img src="img/flags/DE.png" alt="Deutsch"></i>&nbsp;Deutsch</a></li>
+                    <li><a href="#"><i class="flag-icon"><img src="img/flags/IT.png" alt="Italiano"></i>&nbsp;Italiano</a></li>
+                </ul>
+            </div>
+            <div class="toolbar-item hidden-on-mobile"><a href="product-comparison.html">
+                    <div><span class="compare-icon"><i class="icon-repeat"></i><span class="count-label">3</span></span><span class="text-label">Compare</span></div></a></div>
+            <div class="toolbar-item hidden-on-mobile"><a href="account-login.html">
+                    <div><i class="icon-user"></i><span class="text-label">Sign In / Up</span></div></a>
+                <div class="toolbar-dropdown text-center px-3">
+                    <p class="text-xs mb-3 pt-2">Sign in to your account or register new one to have full control over your orders, receive bonuses and more.</p><a class="btn btn-primary btn-sm btn-block" href="account-login.html">Sign In</a>
+                    <p class="text-xs text-muted mb-2">New customer?&nbsp;<a href="account-login.html">Register</a></p>
+                </div>
+            </div>
+            <div class="toolbar-item"><a href="cart.html">
+                    <div><span class="cart-icon"><i class="icon-shopping-cart"></i><span class="count-label">3   </span></span><span class="text-label">Cart</span></div></a>
+                <div class="toolbar-dropdown cart-dropdown widget-cart hidden-on-mobile">
+                    <!-- Entry-->
+                    <div class="entry">
+                        <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/04.jpg" alt="Product"></a></div>
+                        <div class="entry-content">
+                            <h4 class="entry-title"><a href="shop-single.html">Canon EOS M50 Mirrorless Camera</a></h4><span class="entry-meta">1 x $910.00</span>
+                        </div>
+                        <div class="entry-delete"><i class="icon-x"></i></div>
+                    </div>
+                    <!-- Entry-->
+                    <div class="entry">
+                        <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/05.jpg" alt="Product"></a></div>
+                        <div class="entry-content">
+                            <h4 class="entry-title"><a href="shop-single.html">Apple iPhone X 256 GB Space Gray</a></h4><span class="entry-meta">1 x $1,450.00</span>
+                        </div>
+                        <div class="entry-delete"><i class="icon-x"></i></div>
+                    </div>
+                    <!-- Entry-->
+                    <div class="entry">
+                        <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/06.jpg" alt="Product"></a></div>
+                        <div class="entry-content">
+                            <h4 class="entry-title"><a href="shop-single.html">HP LaserJet Pro Laser Printer</a></h4><span class="entry-meta">1 x $188.50</span>
+                        </div>
+                        <div class="entry-delete"><i class="icon-x"></i></div>
+                    </div>
+                    <div class="text-right">
+                        <p class="text-gray-dark py-2 mb-0"><span class='text-muted'>Subtotal:</span> &nbsp;$2,548.50</p>
+                    </div>
+                    <div class="d-flex">
+                        <div class="pr-2 w-50"><a class="btn btn-secondary btn-sm btn-block mb-0" href="cart.html">Expand Cart</a></div>
+                        <div class="pl-2 w-50"><a class="btn btn-primary btn-sm btn-block mb-0" href="checkout.html">Checkout</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Mobile Menu-->
+       @include('page.components.mobile_menu')
+    </div>
+    <!-- Navbar-->
+    <div class="navbar">
+        <div class="btn-group categories-btn">
+            <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"><i class="icon-menu text-lg"></i>&nbsp;Categories</button>
+            <div class="dropdown-menu mega-dropdown">
+                <div class="row">
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/01.jpg"><span class="text-gray-dark">Computers &amp; Accessories</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/02.jpg"><span class="text-gray-dark">Smartphones &amp; Tablets</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/03.jpg"><span class="text-gray-dark">TV, Video &amp; Audio</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/04.jpg"><span class="text-gray-dark">Cameras, Photo &amp; Video</span></a></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/05.jpg"><span class="text-gray-dark">Headphones</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/06.jpg"><span class="text-gray-dark">Wearable Electronics</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/07.jpg"><span class="text-gray-dark">Printers &amp; Ink</span></a></div>
+                    <div class="col-sm-3"><a class="d-block navi-link text-center mb-30" href="shop-grid-ls.html"><img class="d-block" src="img/shop/header-categories/08.jpg"><span class="text-gray-dark">Video Games</span></a></div>
+                </div>
+            </div>
+        </div>
+        <!-- Main Navigation-->
+        @include('page.components.menu')
+        <!-- Toolbar ( Put toolbar here only if you enable sticky navbar )-->
+        <div class="toolbar">
+            <div class="toolbar-inner">
+                <div class="toolbar-item"><a href="product-comparison.html">
+                        <div><span class="compare-icon"><i class="icon-repeat"></i><span class="count-label">3</span></span><span class="text-label">Compare</span></div></a></div>
+                <div class="toolbar-item"><a href="cart.html">
+                        <div><span class="cart-icon"><i class="icon-shopping-cart"></i><span class="count-label">3   </span></span><span class="text-label">Cart</span></div></a>
+                    <div class="toolbar-dropdown cart-dropdown widget-cart">
+                        <!-- Entry-->
+                        <div class="entry">
+                            <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/04.jpg" alt="Product"></a></div>
+                            <div class="entry-content">
+                                <h4 class="entry-title"><a href="shop-single.html">Canon EOS M50 Mirrorless Camera</a></h4><span class="entry-meta">1 x $910.00</span>
+                            </div>
+                            <div class="entry-delete"><i class="icon-x"></i></div>
+                        </div>
+                        <!-- Entry-->
+                        <div class="entry">
+                            <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/05.jpg" alt="Product"></a></div>
+                            <div class="entry-content">
+                                <h4 class="entry-title"><a href="shop-single.html">Apple iPhone X 256 GB Space Gray</a></h4><span class="entry-meta">1 x $1,450.00</span>
+                            </div>
+                            <div class="entry-delete"><i class="icon-x"></i></div>
+                        </div>
+                        <!-- Entry-->
+                        <div class="entry">
+                            <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/06.jpg" alt="Product"></a></div>
+                            <div class="entry-content">
+                                <h4 class="entry-title"><a href="shop-single.html">HP LaserJet Pro Laser Printer</a></h4><span class="entry-meta">1 x $188.50</span>
+                            </div>
+                            <div class="entry-delete"><i class="icon-x"></i></div>
+                        </div>
+                        <div class="text-right">
+                            <p class="text-gray-dark py-2 mb-0"><span class='text-muted'>Subtotal:</span> &nbsp;$2,548.50</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="pr-2 w-50"><a class="btn btn-secondary btn-sm btn-block mb-0" href="cart.html">Expand Cart</a></div>
+                            <div class="pl-2 w-50"><a class="btn btn-primary btn-sm btn-block mb-0" href="checkout.html">Checkout</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
