abdd4058095038861f472a68dc67cc7687d256d6
Davide Entratici <davideentratici@gmail.com>
2018-08-01 16:44:37 +0200

    Aggiunto campi a tabella Client

--- database/migrations/2018_08_01_144003_create_client_table.php
+++ database/migrations/2018_08_01_144003_create_client_table.php
@@ -15,6 +15,20 @@ class CreateClientTable extends Migration
     {
         Schema::create('client', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 255);
+            $table->string('surname', 255);
+            $table->string('email')->unique();
+            $table->string('password', 255);
+            $table->string('phone', 45);
+            $table->string('vat', 255);
+            $table->string('company', 255);
+            $table->string('country', 255);
+            $table->string('route', 255);
+            $table->string('zip_code', 255);
+            $table->string('state', 255);
+            $table->string('maps', 255);
+            $table->string('payment_method', 255);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
