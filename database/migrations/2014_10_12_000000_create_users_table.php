1268615aaf8f6ec44b8c51f0e48ccd8c5dba5102
Davide Entratici <davideentratici@gmail.com>
2018-08-02 11:25:26 +0200

    Modificato campi tabella Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,18 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255);
-            $table->string('surname', 255);
             $table->string('email')->unique();
             $table->string('password', 255);
-            $table->string('img', 255);
-            $table->string('phone', 45);
-            $table->enum('sex', ['m', 'f']);
-            $table->date('birthday');
-            $table->string('country', 255);
-            $table->string('route', 255);
-            $table->string('zip_code', 255);
-            $table->string('state', 255);
-            $table->string('payment_method', 255);
             $table->rememberToken();
             $table->timestamps();
         });
