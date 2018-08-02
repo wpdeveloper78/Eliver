118b3a58c28e6ea2306bf74474259eff991a2acd
Davide Entratici <davideentratici@gmail.com>
2018-08-02 16:51:23 +0200

    Creato array per photo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,8 @@
 
 use Faker\Generator as Faker;
 use App\User;
-
+use App\Models\Album;
+use App\Models\Photo;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -29,6 +30,33 @@ $factory->define(App\Models\Album::class, function (Faker $faker) {
         'album_name' => $faker->name,
         'description' => $faker->text(128),
         'user_id' => User::inRandomOrder()->first()->id
+
+    ];
+});
+
+$factory->define(App\Models\Photo::class, function (Faker $faker) {
+
+    $cats =[
+        'abstract',
+        'animals',
+        'business',
+        'cats',
+        'city',
+        'food',
+        'nightlife',
+        'fashion',
+        'people',
+        'nature',
+        'sports',
+        'technics',
+        'transport',
+    ];
+
+    return [
+        'album_id' => Album::inRandomOrder()->first()->id,
+        'name' => $faker->text(64),
+        'description' => $faker->text(128),
+        'img_path' => $faker->imageUrl(640, 480, $faker->randomElement($cats))
 
     ];
 });
